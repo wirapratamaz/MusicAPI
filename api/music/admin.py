1814934defab21df3094caaf02e3c84c46e04bbc
wirapratamaz <wirabagus185@gmail.com>
2023-01-16 20:58:43 +0800

    add models album

--- api/music/admin.py
+++ api/music/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Songs
+from .models import Songs, Album
 
 # Register your models here.
 admin.site.register(Songs)
+admin.site.register(Album)
--- api/music/models.py
+++ api/music/models.py
@@ -10,3 +10,11 @@ class Songs(models.Model):
     
     def __str__(self):
         return "{} - {}".format(self.id, self.title, self.artist)
+    
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    date_publish = models.DateField()
+    
+    def __str__(self):
+        return "{} - {}".format(self.id, self.name, self.date_publish)
